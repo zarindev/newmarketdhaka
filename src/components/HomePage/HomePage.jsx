5fff34191d12c1261b3851abff7540ac54640e4b
sadmann7 <sadmann898@gmail.com>
2022-07-03 22:12:06 +0600

    changes-three

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import Hero from '../Hero/Hero'
-import CategoryNav from '../Navigation/CategoryNav/CategoryNav'
-import TopNav from '../Navigation/TopNav/TopNav'
-import SearchBox from '../SearchBox/SearchBox'
+import React from 'react';
+import Hero from '../Hero/Hero';
+import CategoryNav from '../Navigation/CategoryNav/CategoryNav';
+import TopNav from '../Navigation/TopNav/TopNav';
+import SearchBox from '../SearchBox/SearchBox';
+import Services from '../Services/Services';
 
 function HomePage() {
   return (
     <>
-    <TopNav/>
-    <CategoryNav/>
-    <Hero/>
-    <SearchBox/>
+      <TopNav />
+      <CategoryNav />
+      <Hero />
+      <SearchBox />
+      <Services />
     </>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
