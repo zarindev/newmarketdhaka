c58c8f553c7732b9e1e93fa992a7e87347b7148a
sadman <sadmann898@gmail.com>
2022-09-02 13:28:32 +0600

    :zap: enable-disable

--- src/pages/ManageServices/TableBlock.jsx
+++ src/pages/ManageServices/TableBlock.jsx
@@ -1,24 +1,15 @@
-import Loading from '../../components/Loading/Loading';
 import Table from '../../components/Table/Table';
-import { useComTableColumn } from '../../hooks/useComTableColumn';
-import { useFilter } from '../../hooks/useFilter';
-import { useFind } from '../../hooks/useFind';
 
-const TableBlock = ({ tableTitle }) => {
-  const { activeCom } = useFind();
-  const activeComId = activeCom.id;
-  const { activeSer, serIsLoading } = useFilter('companyInfoId', activeComId);
-
-  const columns = useComTableColumn();
-
+const TableBlock = ({ tableTitle, activeSer, columns, activeComId }) => {
   return (
     <>
       <h4 className="service-table-title">{tableTitle}</h4>
-      {serIsLoading ? (
-        <Loading color="#ce2d4f" size={115} />
-      ) : (
-        <Table columns={columns} data={activeSer} activeComId={activeComId} />
-      )}
+      <Table
+        columns={columns}
+        data={activeSer}
+        activeComId={activeComId}
+        tableTitle={tableTitle}
+      />
     </>
   );
 };
