199785bd0d548cb773da1dfc876e29469f640497
sadman <sadmann898@gmail.com>
2022-09-21 12:40:56 +0600

    :closed_lock_with_key: environment variables configuration

--- src/context/AppProvider.jsx
+++ src/context/AppProvider.jsx
@@ -6,11 +6,6 @@ const AppContext = createContext();
 const AppProvider = ({ children }) => {
   const [componentFiles, setComponentFiles] = useState([]);
 
-  const serGet = `http://mdadmin-001-site2.ftempurl.com/api/Servivce/GetServiceList`;
-  const comGet = `http://mdadmin-001-site2.ftempurl.com/api/Servivce/GetServiceCompList`;
-  const serPost = `http://mdadmin-001-site2.ftempurl.com/api/Servivce/PotService`;
-  const comPost = `http://mdadmin-001-site2.ftempurl.com/api/Servivce/PotCompany`;
-
   const { mergedSerType } = useMerge();
   const { mergedSerTypeAll } = useMerge();
 
@@ -21,10 +16,6 @@ const AppProvider = ({ children }) => {
         mergedSerTypeAll,
         componentFiles,
         setComponentFiles,
-        serGet,
-        comGet,
-        serPost,
-        comPost,
       }}
     >
       {children}
